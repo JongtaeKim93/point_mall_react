0ef10a084420dc25fe1c0f58cba4968e38ca372d
jongtaekim <minahn93@gmail.com>
2019-08-06 14:29:11 +0900

    commit

--- src/DataHelper.js
+++ src/DataHelper.js
@@ -1,16 +1,54 @@
-class DataHelper {
+import { decorate, observable, autorun } from 'mobx';
 
-    static baseURL() {
+let instance;
+class DataHelper {
+    authToken = '';
+
+    constructor(){
+        if(instance) return instance;
+        instance = this;
+    }
+
+    baseURL() {
         return 'http://localhost:8002';
     }
 
-    static setAuthToken(token) {
+    setAuthToken(token) {
+        this.authToken = token.token_type + ' ' + token.access_token;
         localStorage.setItem('auth_token', 'Bearer ' + token.access_token);
     }
 
+    getAuthToken(){
+        if(this.authToken == null){
+            this.authToken = localStorage.getItem('auth_token')
+        }
+        return this.authToken;
+    }
+
+    static baseURL() {
+        const dataHelper = new DataHelper();
+        return dataHelper.baseURL();
+    }
+
+    static setAuthToken(token) {
+        const dataHelper = new DataHelper();
+        dataHelper.setAuthToken(token);
+    }
+
     static getAuthToken() {
-        return localStorage.getItem('auth_token');
+        const dataHelper = new DataHelper();
+        return dataHelper.getAuthToken();
     }
 }
+
+decorate(DataHelper, {
+    authToken: observable
+});
+
+const helper = new DataHelper();
+
+autorun(() => {
+    console.log(helper.authToken);
+});
 
 export default DataHelper;
