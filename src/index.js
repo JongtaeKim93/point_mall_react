26407f4c78c30c843e1545fc6763c20ea19a0318
jongtaekim <minahn93@gmail.com>
2019-08-07 11:13:06 +0900

    add ItemStore.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'mobx-react';
 import AuthStore from './AuthStore';
+import ItemStore from './ItemStore';
 
 const authStore = new AuthStore();
+const itemSotre = new ItemStore();
 
 ReactDOM.render(
-    <Provider authStore={authStore}>
+    <Provider authStore={authStore} itemStore={itemSotre}>
         <App />
     </Provider>,
      document.getElementById('root')
